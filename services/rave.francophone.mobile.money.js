d3d24c170a924204319d4a03540d7e644b1657f6
Cornelius Ashley-Osuzoka <59456456+Corvus97@users.noreply.github.com>
2021-03-04 13:41:14 +0100

    masked sample public key in franco service

--- services/rave.francophone.mobile.money.js
+++ services/rave.francophone.mobile.money.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 
 
 var spec = morx.spec()
-    .build('PBFPubKey', 'required:false, eg:FLWPUBK-e634d14d9ded04eaf05d5b63a0a06d2f-X')
+    .build('PBFPubKey', 'required:false, eg:FLWPUBK-xxxxxxxxxxxxxxxxxxxxx-X')
     .build('currency', 'required:true, eg:XAF')
     .build('country', 'required:true, eg:NG')
     .build('payment_type', 'required:true, eg:mobilemoneyfranco')
